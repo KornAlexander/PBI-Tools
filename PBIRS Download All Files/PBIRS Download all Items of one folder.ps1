a159bd5d80d69d09e2432f015a533b9d385c357e
KornAlexander <92106145+KornAlexander@users.noreply.github.com>
2024-02-12 11:23:38 +0100

    Add files via upload

---
+++ PBIRS Download All Files/PBIRS Download all Items of one folder.ps1
@@ -0,0 +1,14 @@
+﻿$removepath = 'C:\Users\Username\Targetfolder' 
+
+#clean up the folder
+Get-ChildItem -Path $removepath -File -Recurse | Remove-Item -Verbose
+
+#General Variables
+$PortalURL = 'http://reportserverurl/ReportServer'
+
+#Variables for source and target folders
+$Target_01= 'C:\Users\Targetfolder' 
+$Source_01= '/Foldername -->UseHTMLEncodedFolderName' 
+
+#download items
+Out-RsRestFolderContent -ReportPortalUri $PortalURL -RsFolder $Source_01 -Recurse -Destination $Target_01
---
+++ PBIRS Download All Files/PBIRS Get all Folder Names.ps1
@@ -0,0 +1,30 @@
+﻿#Set Global Variables
+$PortalURL = "http://reporturl/ReportServer";
+$FormattedInventory = @();
+
+#Variables for source and target folders
+$Target_01= "C:\Users\targetfilename.csv";
+$Source_01= '/Foldername'
+ 
+$PBIReports = Get-RsFolderContent -ReportServerUri $PortalURL -RsFolder $ReportFolder -Recurse | Where-Object {$_.TypeName -eq "Folder"}
+ 
+ 
+foreach($report in $PBIReports)
+{
+    #Friendly formatting
+    $fullURL = $PortalURL + "/powerbi" + $report.Path;
+    $created = $report.CreationDate.ToShortDateString() + " " + $report.CreationDate.ToShortTimeString();
+    $modified = $report.ModifiedDate.ToShortDateString() + " " + $report.ModifiedDate.ToShortTimeString();
+    $hidden = "N";
+    $description = $null;
+ 
+    $newPBIReport = New-Object –TypeName PSObject;
+    $newPBIReport | Add-Member –MemberType NoteProperty –Name ReportName –Value $report.Name;
+    $newPBIReport | Add-Member –MemberType NoteProperty –Name ReportDescription –Value $report.Description;
+    $newPBIReport | Add-Member –MemberType NoteProperty –Name ModifiedBy –Value $report.ModifiedBy;
+    $newPBIReport | Add-Member –MemberType NoteProperty –Name Modified –Value $modified;
+ 
+    $FormattedInventory += $newPBIReport;
+}
+ 
+$FormattedInventory | Export-CSV -LiteralPath $Target_01 -NoTypeInformation
